d9b05180ebebfbc74406b546cab04340b2ade742
David Alger <davidmalger@gmail.com>
2019-05-29 09:05:12 -0500

    Rebuilt how usage command works to support command specific help text

---
+++ commands/usage.cmd
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+[[ ! ${WARDEN_COMMAND} ]] && >&2 echo -e "\033[31mThis script is not intended to be run directly!" && exit 1
+
+## load usage info for the given command falling back on default usage text
+if [[ -f "${WARDEN_DIR}/commands/${WARDEN_COMMAND}.help" ]]; then
+  source "${WARDEN_DIR}/commands/${WARDEN_COMMAND}.help"
+else
+  source "${WARDEN_DIR}/commands/usage.help"
+fi
+
+echo -e "${WARDEN_USAGE}"
+exit 1
