f40d6e89ba3a5e8f924f82f2d6da7923995e1a96
David Alger <davidmalger@gmail.com>
2019-05-29 22:55:56 -0500

    Configured traefik with ssl termination for *.warden.test (assuming the cert is generated)

--- commands/up.cmd
+++ commands/up.cmd
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 [[ ! ${WARDEN_COMMAND} ]] && >&2 echo -e "\033[31mThis script is not intended to be run directly!" && exit 1
 
+mkdir -p ~/.warden/etc/traefik
+cp "${WARDEN_DIR}/etc/traefik/traefik.toml" ~/.warden/etc/traefik/traefik.toml
+
 pushd "${WARDEN_DIR}" >/dev/null
 docker-compose -p warden -f docker/docker-compose.yml up -d
