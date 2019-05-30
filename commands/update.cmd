411b36447de3144bf5ac9f867cd4831af33eb8bf
David Alger <davidmalger@gmail.com>
2019-05-30 10:45:08 -0500

    Removed commands setup for building framework and currently do nothing

--- commands/update.cmd
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-[[ ! ${WARDEN_COMMAND} ]] && >&2 echo -e "\033[31mThis script is not intended to be run directly!" && exit 1
-
-echo -e "\033[33mCommand '${WARDEN_COMMAND}' not yet implemented."
