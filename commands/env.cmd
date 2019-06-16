89b9dde56b0a953c59092f16bd3e3c8c22dc5ffe
David Alger <davidmalger@gmail.com>
2019-06-15 20:54:00 -0500

    Added start-sync / stop-sync commands for running mutagen sessions for Magento 2 environments

--- commands/env.cmd
+++ commands/env.cmd
@@ -28,8 +28,34 @@ if (( ${#WARDEN_PARAMS[@]} == 0 )); then
   exit -1
 fi
 
-docker-compose \
-    --project-directory "${WARDEN_ENV_PATH}" \
-    -p "${WARDEN_ENV_NAME}" \
-    -f "${WARDEN_DIR}/environments/${WARDEN_ENV_TYPE}.yml" \
-    "${WARDEN_PARAMS[@]}" "$@"
+## env sub-command execution
+case "${WARDEN_PARAMS[0]}" in
+    start-sync)
+        ## if no mutagen configuration file exists for the environment type, exit with error
+        [[ ! -f "${WARDEN_DIR}/environments/${WARDEN_ENV_TYPE}.toml" ]] \
+            && >&2 echo -e "\033[31mMutagen configuration does not exist for environment type \"${WARDEN_ENV_TYPE}\"" && exit 1
+
+        ## start mutagen daemon if not already running
+        mutagen daemon start
+
+        ## terminate any existing sessions with matching env label
+        mutagen terminate --label-selector "warden-sync=${WARDEN_ENV_NAME}"
+
+        ## create sync session based on environment type configuration
+        mutagen create -c "${WARDEN_DIR}/environments/${WARDEN_ENV_TYPE}.toml" \
+            --label "warden-sync=${WARDEN_ENV_NAME}" \
+            "${WARDEN_ENV_PATH}" "docker://$(warden env ps -q php-fpm)/var/www/html"
+        ;;
+    stop-sync)
+        ## terminate only sessions labeled with this env name
+        mutagen terminate --label-selector "warden-sync=${WARDEN_ENV_NAME}"
+        ;;
+    *)
+        ## anything not caught above is simply passed through to docker-compose to orchestrate
+        docker-compose \
+            --project-directory "${WARDEN_ENV_PATH}" \
+            -p "${WARDEN_ENV_NAME}" \
+            -f "${WARDEN_DIR}/environments/${WARDEN_ENV_TYPE}.yml" \
+            "${WARDEN_PARAMS[@]}" "$@"
+        ;;
+esac
