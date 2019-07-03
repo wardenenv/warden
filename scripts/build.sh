049c6eb0489f67e91adab7de39cd5e350d08fbcb
David Alger <davidmalger@gmail.com>
2019-07-02 20:37:09 -0500

    Updated build script to only push when --push is passed as first argument

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,17 +13,24 @@ readonly BASE_DIR="$(
   && pwd
 )/.."
 
-## support passing a single argument to the command to be more specific on what is built
-SEARCH_PATH="${1:-*}"
+## if --push is passed as first argument to script, this will login to docker hub and push images
+PUSH_FLAG=
+if [[ "${1:-}" = "--push" ]]; then
+  PUSH_FLAG=1
+  SEARCH_PATH="${2:-*}"
+else
+  SEARCH_PATH="${1:-*}"
+fi
 
 ## change into base directory and login to docker hub if neccessary
 pushd ${BASE_DIR} >/dev/null
-docker login
+[[ $PUSH_FLAG ]] && docker login
 
 ## iterate over and build each Dockerfile
 for file in $(find ${SEARCH_PATH} -type f -name Dockerfile); do
     BUILD_DIR="$(dirname "${file}")"
-    IMAGE_TAG="davidalger/warden:$(dirname "${file}" | tr / -)"
+    IMAGE_TAG="davidalger/warden:$(dirname "${file}" | tr / - | sed 's/--/-/')"
+
     docker build -t "${IMAGE_TAG}" ${BUILD_DIR}
-    docker push "${IMAGE_TAG}"
+    [[ $PUSH_FLAG ]] && docker push "${IMAGE_TAG}"
 done
