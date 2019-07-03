be47e3f8cb6f9e0e97ff0f4c3a097023712303cb
David Alger <davidmalger@gmail.com>
2019-07-03 17:56:04 -0500

    Added `warden shell` command for easily dropping into the `php-fpm` container

---
+++ commands/shell.cmd
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+[[ ! ${WARDEN_COMMAND} ]] && >&2 echo -e "\033[31mThis script is not intended to be run directly!" && exit 1
+
+source "${WARDEN_DIR}/utils/env.sh"
+WARDEN_ENV_PATH="$(locateEnvPath)" || exit $?
+loadEnvConfig "${WARDEN_ENV_PATH}" || exit $?
+
+## set defaults for this command which can be overriden either using exports in the user
+## profile or setting them in the .env configuration on a per-project basis
+WARDEN_ENV_SHELL_COMMAND=${WARDEN_ENV_SHELL_COMMAND:-bash}
+WARDEN_ENV_SHELL_CONTAINER=${WARDEN_ENV_SHELL_CONTAINER:-php-fpm}
+
+## simply allow the return code from sub-command to bubble up per normal
+trap '' ERR
+
+"${WARDEN_DIR}/bin/warden" env exec "${WARDEN_ENV_SHELL_CONTAINER}" \
+    "${WARDEN_ENV_SHELL_COMMAND}" "${WARDEN_PARAMS[@]}" "$@"
