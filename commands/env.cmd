5635bec17ed3d3f92d7ec2b5fe8500e28acdbd41
Łukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2019-11-21 00:56:04 +0100

    Add `WARDEN_SELENIUM` option to install Selenium Standalone which is compatible with Magento 2 MFTF

--- commands/env.cmd
+++ commands/env.cmd
@@ -53,6 +53,11 @@ if [[ -f "${WARDEN_ENV_PATH}/.warden/warden-env.${WARDEN_ENV_SUBT}.yml" ]]; then
     DOCKER_COMPOSE_ARGS+=("${WARDEN_ENV_PATH}/.warden/warden-env.${WARDEN_ENV_SUBT}.yml")
 fi
 
+if [[ ${WARDEN_SELENIUM} -eq 1 && -f "${WARDEN_DIR}/environments/${WARDEN_ENV_TYPE}.selenium.base.yml" ]]; then
+    DOCKER_COMPOSE_ARGS+=("-f")
+    DOCKER_COMPOSE_ARGS+=("${WARDEN_DIR}/environments/${WARDEN_ENV_TYPE}.selenium.base.yml")
+fi
+
 ## lookup internal (warden docker network) IP address of traefik container (do not fail if traefik is stopped)
 export TRAEFIK_ADDRESS="$(docker container inspect traefik \
     --format '{{.NetworkSettings.Networks.warden.IPAddress}}' 2>/dev/null || true)"
