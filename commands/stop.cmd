8636bab91d707884afa1435873fd2bf810fade10
David Alger <davidmalger@gmail.com>
2020-03-19 11:52:11 -0500

    Updated CLI color resets to be macOS compatible adding it to utils/*.sh as well

--- commands/stop.cmd
+++ commands/stop.cmd
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-[[ ! ${WARDEN_COMMAND} ]] && >&2 echo -e "\033[31mThis script is not intended to be run directly!\e[0m" && exit 1
+[[ ! ${WARDEN_COMMAND} ]] && >&2 echo -e "\033[31mThis script is not intended to be run directly!\033[0m" && exit 1
 
 pushd "${WARDEN_HOME_DIR}" >/dev/null
 docker-compose -p warden -f "${WARDEN_DIR}/docker/docker-compose.yml" stop "${WARDEN_PARAMS[@]}" "$@"
--- commands/version.cmd
+++ commands/version.cmd
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-[[ ! ${WARDEN_COMMAND} ]] && >&2 echo -e "\033[31mThis script is not intended to be run directly!\e[0m" && exit 1
+[[ ! ${WARDEN_COMMAND} ]] && >&2 echo -e "\033[31mThis script is not intended to be run directly!\033[0m" && exit 1
 
 cat "${WARDEN_DIR}/version"
