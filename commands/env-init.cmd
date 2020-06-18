1e1bc751335f799cb7b4d5e38f864f91a5eae9e5
Myles Forrest <lunaetic@lunaecy.com>
2020-06-18 11:57:48 -0400

    Add bash scripting to prompt user if there is an extant .env file when running env-init

--- commands/env-init.cmd
+++ commands/env-init.cmd
@@ -4,7 +4,17 @@
 source "${WARDEN_DIR}/utils/env.sh"
 WARDEN_ENV_PATH="$(pwd -P)"
 
-# TODO: If the .env file already exists; prompt user instead of overwriting
+if test -f "${WARDEN_ENV_PATH}/.env"; then
+  while true; do
+    read -p $'\033[32mA warden env file already exists at '"${WARDEN_ENV_PATH}/.env"$'; would you like to overwrite? y/n\033[0m ' resp
+    case $resp in
+      [Yy]*) echo "Overwriting extant .env file"; break;;
+      [Nn]*) exit;;
+      *) echo "Please answer (y)es or (n)o";;
+    esac
+  done
+fi
+
 # TODO: Prompt user for inputs when arguments remain unspecified
 
 WARDEN_ENV_NAME="${WARDEN_PARAMS[0]:-}"
@@ -112,10 +122,10 @@ if [[ "${WARDEN_ENV_TYPE}" == "laravel" ]]; then
 
 		CACHE_DRIVER=redis
 		SESSION_DRIVER=redis
-		
+
 		REDIS_HOST=redis
 		REDIS_PORT=6379
-		
+
 		MAIL_DRIVER=sendmail
 	EOT
 fi
