71c996eb34901dfeef9b76e7d5784013d3c71795
David Alger <davidmalger@gmail.com>
2020-06-19 20:33:32 -0500

    Fixed incorrect indetation in traefik config generated by new svc command

--- commands/svc.cmd
+++ commands/svc.cmd
@@ -45,18 +45,18 @@ if [[ "${WARDEN_PARAMS[0]}" == "up" ]]; then
     ## generate dynamic traefik ssl termination configuration
     cat > "${WARDEN_HOME_DIR}/etc/traefik/dynamic.yml" <<-EOT
 		tls:
-		stores:
+		  stores:
 		    default:
 		    defaultCertificate:
-		        certFile: /etc/ssl/certs/${WARDEN_SERVICE_DOMAIN}.crt.pem
-		        keyFile: /etc/ssl/certs/${WARDEN_SERVICE_DOMAIN}.key.pem
-		certificates:
+		      certFile: /etc/ssl/certs/${WARDEN_SERVICE_DOMAIN}.crt.pem
+		      keyFile: /etc/ssl/certs/${WARDEN_SERVICE_DOMAIN}.key.pem
+		  certificates:
 	EOT
 
     for cert in $(find "${WARDEN_SSL_DIR}/certs" -type f -name "*.crt.pem" | sed -E 's#^.*/ssl/certs/(.*)\.crt\.pem$#\1#'); do
         cat >> "${WARDEN_HOME_DIR}/etc/traefik/dynamic.yml" <<-EOF
 		    - certFile: /etc/ssl/certs/${cert}.crt.pem
-		    keyFile: /etc/ssl/certs/${cert}.key.pem
+		      keyFile: /etc/ssl/certs/${cert}.key.pem
 		EOF
     done
 
