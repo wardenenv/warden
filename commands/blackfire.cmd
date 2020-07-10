8e51989dcb1e1139f7983f8d9b7aeb6a917111e8
David Alger <davidmalger@gmail.com>
2020-07-10 18:10:19 -0500

    Corrected variable names in 'blackfire' command

--- commands/blackfire.cmd
+++ commands/blackfire.cmd
@@ -6,11 +6,11 @@ loadEnvConfig "${WARDEN_ENV_PATH}" || exit $?
 
 ## set defaults for this command which can be overriden either using exports in the user
 ## profile or setting them in the .env configuration on a per-project basis
-WARDEN_ENV_PROFILE_COMMAND=${WARDEN_ENV_PROFILE_COMMAND:-blackfire}
-WARDEN_ENV_PROFILE_CONTAINER=${WARDEN_ENV_PROFILE_CONTAINER:-php-blackfire}
+WARDEN_ENV_BLACKFIRE_COMMAND=${WARDEN_ENV_BLACKFIRE_COMMAND:-blackfire}
+WARDEN_ENV_BLACKFIRE_CONTAINER=${WARDEN_ENV_BLACKFIRE_CONTAINER:-php-blackfire}
 
 ## allow return codes from sub-process to bubble up normally
 trap '' ERR
 
-"${WARDEN_DIR}/bin/warden" env exec "${WARDEN_ENV_PROFILE_CONTAINER}" \
-    "${WARDEN_ENV_PROFILE_COMMAND}" "${WARDEN_PARAMS[@]}" "$@"
+"${WARDEN_DIR}/bin/warden" env exec "${WARDEN_ENV_BLACKFIRE_CONTAINER}" \
+    "${WARDEN_ENV_BLACKFIRE_COMMAND}" "${WARDEN_PARAMS[@]}" "$@"
