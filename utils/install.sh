faa3918ce027680e6c9667021434fc8c1be2bd0a
rbouma <14012592+rbouma@users.noreply.github.com>
2022-02-22 11:12:19 +0100

    Revert changes to install.sh

--- utils/install.sh
+++ utils/install.sh
@@ -7,7 +7,6 @@ function installSshConfig () {
     echo "    Note: This addition to the ssh_config file can sometimes be erased by a system"
     echo "    upgrade requiring reconfiguring the SSH config for tunnel.warden.test."
     cat <<-EOT | sudo tee -a /etc/ssh/ssh_config >/dev/null
-
 			## WARDEN START ##
 			Host tunnel.warden.test
 			HostName 127.0.0.1
@@ -17,15 +16,6 @@ function installSshConfig () {
 			## WARDEN END ##
 			EOT
   fi
-
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-         if ! grep '## WARDEN START ##' ~/.ssh/config >/dev/null; then
-             echo "==> Configuring sshd tunnel for mac os in user ssh config (used for tableplus command)"
-             echo "    Note: This addition to the ssh_config file can sometimes be erased by a system"
-             echo "    upgrade requiring reconfiguring the SSH config for tunnel.warden.test."
-             sed -i.old '1s;^;## WARDEN START ##\nHost tunnel.warden.test\nHostName 127.0.0.1\nUser user\nPort 2222\nIdentityFile ~/.warden/tunnel/ssh_key\n## WARDEN END ##\n;' ~/.ssh/config
-           fi
-      fi
 }
 
 function assertWardenInstall {
