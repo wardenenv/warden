f54aa952e97e5838144500f5ab8234dad2c8ad03
Navarr <navarr@swiftotter.com>
2022-09-01 09:12:05 -0400

    Make ElasticHQ optional and default to off

--- commands/env.cmd
+++ commands/env.cmd
@@ -77,6 +77,9 @@ fi
 [[ ${WARDEN_ELASTICSEARCH} -eq 1 ]] \
     && appendEnvPartialIfExists "elasticsearch"
 
+[[ ${WARDEN_ELASTICHQ:=1} -eq 1 ]] \
+    && appendEnvPartialIfExists "elastichq"
+
 [[ ${WARDEN_OPENSEARCH} -eq 1 ]] \
     && appendEnvPartialIfExists "opensearch"
 
