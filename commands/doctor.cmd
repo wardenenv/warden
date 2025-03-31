c424317d317cd0017f01fbb2f5cb2e5155e065ed
Hardy Johnson <hardyjohnson+github@gmail.com>
2025-03-31 15:36:03 -0700

    Update formatting and information output to include brew info and additional override detection.

--- commands/doctor.cmd
+++ commands/doctor.cmd
@@ -8,37 +8,67 @@
 ## darwin
 # > sw_vers
 
-sw_vers --productName
-sw_vers --productVersion
+if [[ $OSTYPE =~ ^darwin ]]; then
+    echo -e "\033[032mHost information:\033[0m"
+    sw_vers --productName
+    sw_vers --productVersion
+fi
 echo
 
+echo -e "\033[32mOS, version, architecture:\033[0m"
+uname -orm
+echo
 
+echo -e "\033[32mHomebrew information:\033[0m"
+brew config
+echo
+
+echo -e "\033[32mContainer runtime and compose information:\033[0m"
 docker --version
 ${DOCKER_COMPOSE_COMMAND} version
-
 echo
 
-echo "Warden version:" $(${WARDEN_BIN} version)
+echo -e "\033[32mWarden version:\033[0m"
+echo $(${WARDEN_BIN} version)
 echo
 
-echo "Warden image, tag and architecture: "
+echo -e "\033[32mWarden global .env:\033[0m"
+cat ~/.warden/.env
 echo
+
+echo -e "\033[32mWarden service override via Docker compose file:\033[0m"
+if [[ -f ~/.warden/docker-compose.yml ]]; then
+    echo -e "\033[33mWarden services have additional service configuration added or overridden via ~/.warden/docker-compose.yml file.\033[0m"
+else
+    echo -e "\033[33mWarden services do not appear to be overridden via ~/.warden/docker-compose.yml file.\033[0m"
+fi
+echo
+
+echo -e "\033[32mWarden service override via ~/.warden/warden-env.yml partial:\033[0m"
+if [[ -f ~/.warden/warden-env.yml ]]; then
+    echo -e "\033[33mWarden services have additional service configuration added or overridden via ~/.warden/warden-env.yml partial.\033[0m"
+else
+    echo -e "\033[33mWarden services do not appear to be overridden via ~/.warden/warden-env.yml partial.\033[0m"
+fi
+echo
+
+echo -e "\033[32mWarden image, tag and architecture:\033[0m"
 WARDEN_IMAGES=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep -i wardenenv)
 for img in ${WARDEN_IMAGES}; do
     echo $img:$(docker image inspect $img --format "{{.Architecture}}");
 done
 echo
 
+echo -e "\033[32mWarden environments and service configuration files :\033[0m"
 ${WARDEN_BIN} svc ls -a --format json | jq '.[] | (.Name + " - " + .Status), (.ConfigFiles | split(","))'
 echo
 
+echo -e "\033[32mWarden status:\033[0m"
 "$WARDEN_BIN" status
 echo
 
-mutagen sync list
+if [[ ${WARDEN_MUTAGEN_ENABLE} -eq 1 ]]; then
+    echo -e "\033[32mMutagen sync list\033[0m"
+    mutagen sync list
+fi
 echo
-
-# ssh information related warden / warden project
-# any docker inspect or configuration information
-
-# echo "Warden doctor complete!"
