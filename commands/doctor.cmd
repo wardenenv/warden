df5a3c3622e58886a1dc953385a1292bc976f174
Hardy Johnson <hardyjohnson+github@gmail.com>
2025-04-01 13:48:01 -0700

    Cleanup detection of commands, add tests for brew and jq, test if docker is running.

--- commands/doctor.cmd
+++ commands/doctor.cmd
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 [[ ! ${WARDEN_DIR} ]] && >&2 echo -e "\033[31mThis script is not intended to be run directly!\033[0m" && exit 1
 
+## Disable immediate exit on failure (set in main warden bin), we use this to detect whether docker is running and continue.
+set +e
+
+## Allow return codes from sub-process to bubble up normally, necessary for command testing
+trap '' ERR
+
 # The warden doctor command is designed to collect information useful in reasoning about the
 # state of a system and configuration warden is running with.
 
-# Host information
-## darwin
-# > sw_vers
-
-if [[ $OSTYPE =~ ^darwin ]]; then
+if [[ ${OSTYPE} =~ ^darwin ]]; then
     echo -e "\033[032mHost information:\033[0m"
     sw_vers --productName
     sw_vers --productVersion
@@ -19,8 +21,11 @@ echo -e "\033[32mOS, version, architecture:\033[0m"
 uname -orm
 echo
 
-echo -e "\033[32mHomebrew information:\033[0m"
-brew config
+command -v brew &>/dev/null
+if [[ $? -eq 0 ]]; then
+    echo -e "\033[32mHomebrew information:\033[0m"
+    brew config
+fi
 echo
 
 echo -e "\033[32mContainer runtime and compose information:\033[0m"
@@ -29,7 +34,7 @@ ${DOCKER_COMPOSE_COMMAND} version
 echo
 
 echo -e "\033[32mWarden version:\033[0m"
-echo $(${WARDEN_BIN} version)
+echo ${WARDEN_BIN} version
 echo
 
 echo -e "\033[32mWarden global .env:\033[0m"
@@ -52,22 +57,36 @@ else
 fi
 echo
 
-echo -e "\033[32mWarden image, tag and architecture:\033[0m"
-WARDEN_IMAGES=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep -i wardenenv)
-for img in ${WARDEN_IMAGES}; do
-    echo $img:$(docker image inspect $img --format "{{.Architecture}}");
-done
+docker stats --no-stream &>/dev/null
+# Docker is required to be running for the next set of commands
+if [[ $? -eq 0 ]]; then
+    echo -e "\033[32mWarden image, tag and architecture:\033[0m"
+
+    WARDEN_IMAGES=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep -i wardenenv)
+    for img in ${WARDEN_IMAGES}; do
+        echo $img:$(docker image inspect $img --format "{{.Architecture}}");
+    done
+    echo
+
+    echo -e "\033[32mWarden environments and service configuration files:\033[0m"
+    command -v jq >/dev/null
+    if [[ $? -eq 0 ]]
+    then
+        ${WARDEN_BIN} svc ls -a --format json | jq '.[] | (.Name + " - " + .Status), (.ConfigFiles | split(","))'
+    else
+        ${WARDEN_BIN} svc ls -a --format table
+    fi
+    echo
+
+    echo -e "\033[32mWarden status:\033[0m"
+    ${WARDEN_BIN} status
+else
+    echo "Docker does not appear to be running. Start Docker and re-run this command to see Warden images, tags and architecture."
+fi
 echo
 
-echo -e "\033[32mWarden environments and service configuration files :\033[0m"
-${WARDEN_BIN} svc ls -a --format json | jq '.[] | (.Name + " - " + .Status), (.ConfigFiles | split(","))'
-echo
-
-echo -e "\033[32mWarden status:\033[0m"
-"$WARDEN_BIN" status
-echo
-
-if [[ ${WARDEN_MUTAGEN_ENABLE} -eq 1 ]]; then
+command -v mutagen &>/dev/null
+if [[ $? -eq 0 ]]; then
     echo -e "\033[32mMutagen sync list\033[0m"
     mutagen sync list
 fi
